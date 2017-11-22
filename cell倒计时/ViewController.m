e3a29f321a5710bd7d274e508c87768231d9c555
eaph sing <xing.yf@yougou.com>
2017-11-22 10:00:21 +0800

    Initial Commit

---
+++ cell倒计时/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  cell倒计时
+//
+//  Created by Eaph Sing on 2017/11/22.
+//  Copyright © 2017年 Ray Wenderlich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
